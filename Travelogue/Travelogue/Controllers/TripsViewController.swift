29f80632f2f227739ec854e67860b6d060b60a29
Dominic <djpkvf@mail.missouri.edu>
2018-07-23 18:41:11 -0500

    Initial XCode Commit
    Trips TableView is functional

---
+++ Travelogue/Travelogue/Controllers/TripsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TripsViewController.swift
+//  Travelogue
+//
+//  Created by Dominic Pilla on 7/23/18.
+//  Copyright © 2018 Dominic Pilla. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TripsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tripCell", for: indexPath)
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
